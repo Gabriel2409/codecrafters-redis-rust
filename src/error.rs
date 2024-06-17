2dbb772fe4adc52dd2c59f6c590b4090db65b4f3
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-06-17 21:20:41 +0200

    Add an event loop

--- src/error.rs
+++ src/error.rs
@@ -4,5 +4,11 @@ use thiserror::Error;
 pub enum Error {
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    ParseIntError(#[from] std::num::ParseIntError),
+
+    #[error(transparent)]
+    NetAddrParseError(#[from] std::net::AddrParseError),
 }
 pub type Result<T> = core::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,98 @@
 mod error;
 pub use crate::error::{Error, Result};
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
+use std::io::{Read, Write};
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    // size of the buffer is arbitrary
-    let mut buf = [0; 256];
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
 
-    loop {
-        // calling read on stream goes to the next command
-        let bytes_read = stream.read(&mut buf)?;
-
-        // No more command on the connection
-        if bytes_read == 0 {
-            break;
-        }
-
-        stream.write_all(b"+PONG\r\n")?;
-    }
-
-    Ok(())
-}
+// Some tokens to allow us to identify which event is for which socket.
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
 
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // Create a poll instance.
+    let mut poll = Poll::new()?;
+    // Create storage for events.
+    let mut events = Events::with_capacity(128);
 
-    // Uncomment this block to pass the first stage
+    // Setup the server socket.
+    let addr = "127.0.0.1:6379".parse()?;
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let mut server = TcpListener::bind(addr)?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream)?;
+    // Start listening for incoming connections.
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE)?;
+
+    // Setup the client socket.
+    let mut client = TcpStream::connect(addr)?;
+    // Register the socket.
+    poll.registry()
+        .register(&mut client, CLIENT, Interest::READABLE | Interest::WRITABLE)?;
+
+    // Maintain a list of connections.
+    let mut connections = Vec::new();
+
+    loop {
+        // Poll Mio for events, blocking until we get an event.
+        poll.poll(&mut events, None)?;
+
+        // Process each event.
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    // If this is an event for the server, it means a connection is ready to be accepted.
+                    loop {
+                        match server.accept() {
+                            Ok((mut stream, _)) => {
+                                let token = Token(connections.len() + 1);
+                                poll.registry().register(
+                                    &mut stream,
+                                    token,
+                                    Interest::READABLE | Interest::WRITABLE,
+                                )?;
+                                connections.push(stream);
+                            }
+                            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                                // No more connections to accept.
+                                break;
+                            }
+                            Err(e) => {
+                                println!("Error accepting connection: {}", e);
+                            }
+                        }
+                    }
+                }
+                token => {
+                    // Handle events for a connection.
+                    let idx = token.0 - 1;
+                    if let Some(connection) = connections.get_mut(idx) {
+                        handle_connection(connection)?;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    let mut buffer = [0; 512];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => {
+                // Connection was closed by the client.
+                return Ok(());
+            }
+            Ok(n) => {
+                // Echo the data back to the client.
+                stream.write_all(b"+PONG\r\n")?;
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                // No more data to read.
+                break;
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error reading from connection: {}", e);
             }
         }
     }
