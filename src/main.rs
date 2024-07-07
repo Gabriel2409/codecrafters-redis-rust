2351df85d578f47abbbda20a56870c2cea853253
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-07-07 18:07:25 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -195,6 +195,9 @@ fn handle_connection(connection: &mut TcpStream, db: &mut RedisDb) -> Result<(bo
         if let RedisCommand::Psync = redis_command {
             register = true;
             let bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")?;
+
+            std::thread::sleep(Duration::from_millis(200));
+
             connection.write_all(format!("${}\r\n", bytes.len()).as_bytes())?;
             connection.write_all(&bytes)?;
 
