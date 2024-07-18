d493b6e01a42fe055d2c4943360e0eafd9d97930
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-07-18 18:18:36 +0200

    Remove sleep

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -95,8 +95,6 @@ pub fn handle_connection(
                 _ => Err(Error::InvalidAnswerDuringHandshake(redis_value.clone()))?,
             },
             ConnectionState::BeforePsync => {
-                // Avoid jumbled mess
-                std::thread::sleep(Duration::from_millis(25));
                 db.state = ConnectionState::BeforeRdbFile;
             }
             ConnectionState::Waiting(_, _, _, _) => {
